d6bee4ddfd7dc69d91d84c1daaa62d520defdc8a
suvishsv <suvishraj187@gmail.com>
2025-07-03 22:04:55 +0530

    adding new types of validation.eg file validation upto 2 mb

---
+++ success.js
@@ -0,0 +1,42 @@
+const input= document.querySelector('input[type="file"]');
+const errorDiv = document.querySelector('.error');
+
+form.addEventListener('submit',(e)=>{
+   
+    if(validation()){
+         // validation is true.then redirect to another HTML file, e.g., "successpage.html"
+         window.location.href = "successpage.html";
+         e.preventDefault();
+    }
+    else
+    {
+        e.preventDefault();
+    } 
+});
+
+
+
+function validation() {
+    const file= input.files[0];
+    if(!file){
+        const err= new Error("File not selected");
+        // console.error(err);
+        errorDiv.textContent = err.message;
+        return false;
+    }
+
+const limit=2000;
+const fileSize = file.size / 1024; // Convert bytes to KB
+
+if(fileSize > limit){
+    const err=new Error(`File size exceeds ${(fileSize/1000).toFixed(2)} MB`);
+    errorDiv.textContent = err.message;;
+    return false;
+}
+else{
+    userinfo.textContent="File uploaded successfully";
+    return true;
+
+}
+
+}
